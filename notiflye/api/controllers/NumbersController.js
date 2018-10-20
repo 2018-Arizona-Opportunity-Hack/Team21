18839f726656cac7d5abc84e68fc33f33fcce2fb
Zeus Yousif <Zeus_Yousif@Zeus-MacBook-Pro.local>
2018-10-20 13:39:57 -0700

    Fixed NPM Warning

--- notiflye/api/controllers/NumbersController.js
+++ notiflye/api/controllers/NumbersController.js
@@ -4,10 +4,11 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+ // req.session.userId
 
 module.exports = {
   index: function(req, res){
-    res.send('Hello')
+    res.render('index.ejs', {user: req.session.userId})
   }
 
 };
